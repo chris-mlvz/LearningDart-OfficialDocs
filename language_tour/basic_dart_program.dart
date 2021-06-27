6f7e843793912c4bf8cee46a43fa1ee700ff7ad8
chris_mlvz <chris.mlvz@gmail.com>
2021-06-27 15:35:54 -0500

    A basic dart program

---
+++ language_tour/basic_dart_program.dart
@@ -0,0 +1,10 @@
+// Define a function
+void printInteger(int aNumber) {
+  print('The number is $aNumber.'); // Print to console.
+}
+
+// This is where app starts executing
+void main(List<String> args) {
+  var number = 42; // Declare and initialize a variable
+  printInteger(number); // Call a function
+}
