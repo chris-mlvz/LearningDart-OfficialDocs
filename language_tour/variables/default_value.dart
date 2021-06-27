04a5b093f10c2a876a508c9e75c3f1912ff16875
chris_mlvz <chris.mlvz@gmail.com>
2021-06-27 15:43:25 -0500

    variables: default value

---
+++ language_tour/variables/default_value.dart
@@ -0,0 +1,14 @@
+void main(List<String> args) {
+  // int? lineCount;
+  // assert(lineCount == null);
+
+  // int lineCount = 0;
+
+  // int lineCount;
+  // if (weLikeToCount) {
+  //   lineCount = countLines();
+  // } else {
+  //   lineCount = 0;
+  // }
+  // print(lineCount);
+}
