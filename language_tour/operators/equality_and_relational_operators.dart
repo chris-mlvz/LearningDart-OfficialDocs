c60550762e66313c94cb13f976b8a81f412f08ba
chris_mlvz <chris.mlvz@gmail.com>
2021-06-28 16:08:55 -0500

    equality and relational operators

---
+++ language_tour/operators/equality_and_relational_operators.dart
@@ -0,0 +1,9 @@
+void main(List<String> args) {
+  // * Equality and relational operators
+  assert(2 == 2);
+  assert(2 != 3);
+  assert(3 > 2);
+  assert(2 < 3);
+  assert(3 >= 3);
+  assert(2 <= 3);
+}
