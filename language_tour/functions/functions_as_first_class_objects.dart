ada62ae7ec68d81d06949e793d79bf3f7c04c1e9
chris_mlvz <chris.mlvz@gmail.com>
2021-06-27 20:28:44 -0500

    functions ad first class objects

---
+++ language_tour/functions/functions_as_first_class_objects.dart
@@ -0,0 +1,16 @@
+void main(List<String> args) {
+//   var list = [1, 2, 3];
+
+// // Pass printElement as a parameter.
+//   list.forEach(printElement);
+
+  // * assign a function to a variable
+  // var loudify = (msg) => '!!! ${msg.toUpperCase()} !!!';
+  // assert(loudify('hello') == '!!! HELLO !!!');
+  // print(loudify('hello'));
+}
+
+// * function as a parameter
+// void printElement(int element) {
+//   print(element);
+// }
