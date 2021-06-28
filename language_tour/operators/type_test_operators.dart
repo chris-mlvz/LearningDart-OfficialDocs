1dc5271e8dbdb0384426d412ae8d6c947104f4a9
chris_mlvz <chris.mlvz@gmail.com>
2021-06-28 16:11:02 -0500

    type test operators

---
+++ language_tour/operators/type_test_operators.dart
@@ -0,0 +1,9 @@
+void main(List<String> args) {
+  // * cast operator
+  // (employee as Person).firstName = 'Bob';
+
+  // * check the type
+  // if (employee is Person) {
+  // // Type check
+  // employee.firstName = 'Bob';
+}
