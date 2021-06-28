24f8d81280a68be36076f091c193a39badd42178
chris_mlvz <chris.mlvz@gmail.com>
2021-06-27 19:32:34 -0500

    runes

---
+++ language_tour/build_in_types/runes_and_grapheme_clusters.dart
@@ -0,0 +1,7 @@
+// import 'package:characters/characters.dart';
+void main(List<String> args) {
+  // var hi = 'Hi 🇩🇰';
+  // print(hi);
+  // print('The end of the string: ${hi.substring(hi.length - 1)}');
+  // print('The last character: ${hi.characters.last}\n');
+}
