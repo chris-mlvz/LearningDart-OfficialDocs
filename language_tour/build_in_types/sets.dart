2218f01565e9571417d10c7bb3d7cd4727a161be
chris_mlvz <chris.mlvz@gmail.com>
2021-06-27 19:11:48 -0500

    sets

---
+++ language_tour/build_in_types/sets.dart
@@ -0,0 +1,34 @@
+void main(List<String> args) {
+  // * simple Dart set
+  var halogens = {'fluorine', 'chlorine', 'bromine', 'iodine', 'astatine'};
+  // print(halogens);
+
+  // * empty set
+  // var names = <String>{};
+  // Set<String> names = {}; // This works, too.
+  // var names = {}; // Creates a map, not a set.
+
+  // * Add items
+  // var elements = <String>{};
+  // elements.add('fluorine');
+  // elements.addAll(halogens);
+  // print(elements);
+
+  // * number of items
+  // var elements = <String>{};
+  // elements.add('fluorine');
+  // elements.addAll(halogens);
+  // assert(elements.length == 5);
+  // print(elements.length);
+
+  // * set compile-time constant
+  final constantSet = const {
+    'fluorine',
+    'chlorine',
+    'bromine',
+    'iodine',
+    'astatine',
+  };
+  // constantSet.add('helium'); // This line will cause an error.
+  print(constantSet);
+}
