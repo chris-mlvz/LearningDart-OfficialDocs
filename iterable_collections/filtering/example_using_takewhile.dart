da686326e4b8a0451a52065fb0a93b62d7906aeb
chris_mlvz <chris.mlvz@gmail.com>
2021-06-29 14:07:10 -0500

    Example: using takeWhile() and skipWhile().

---
+++ iterable_collections/filtering/example_using_takewhile.dart
@@ -0,0 +1,12 @@
+main() {
+  var numbers = [1, 3, -2, 0, 4, 5];
+
+  var numbersUntilZero = numbers.takeWhile((number) => number != 0);
+  print('Numbers until 0: $numbersUntilZero');
+
+  var numbersAfterZero = numbers.skipWhile((number) => number != 0);
+  print('Numbers after 0: $numbersAfterZero');
+
+  var numbersUntilNegative = numbers.takeWhile((number) => !number.isNegative);
+  print('Numbers until negative: $numbersUntilNegative');
+}
