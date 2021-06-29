f6e37323a3c66868a18d644506bc458fff5437c1
chris_mlvz <chris.mlvz@gmail.com>
2021-06-29 18:25:09 -0500

    while and do while

---
+++ language_tour/control_flow_statements/while_and_do_while.dart
@@ -0,0 +1,11 @@
+void main(List<String> args) {
+  // * A while loop evaluates the condition before the loop
+  // while (!isDone()) {
+  //   doSomething();
+  // }
+
+  // * A do-while loop evaluates the condition after the loop:
+//   do {
+//     printLine();
+//   } while (!atEndOfPage());
+// }
