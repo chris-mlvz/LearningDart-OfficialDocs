04e8c3e8b66b0c55bf81a00d25b2933d25b28dd2
chris_mlvz <chris.mlvz@gmail.com>
2021-06-29 18:17:16 -0500

    if and else

---
+++ language_tour/control_flow_statements/if_and_else.dart
@@ -0,0 +1,10 @@
+void main(List<String> args) {
+  //  * Example if and else
+  // if (isRaining()) {
+  //   you.bringRainCoat();
+  // } else if (isSnowing()) {
+  //   you.wearJacket();
+  // } else {
+  //   car.putTopDown();
+  // }
+}
