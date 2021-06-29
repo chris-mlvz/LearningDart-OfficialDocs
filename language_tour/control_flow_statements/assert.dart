f28863069c93640731d7fd4a2c5e6c08604b1cb7
chris_mlvz <chris.mlvz@gmail.com>
2021-06-29 18:41:48 -0500

    asserts

---
+++ language_tour/control_flow_statements/assert.dart
@@ -0,0 +1,17 @@
+void main(List<String> args) {
+  // * Assert disrip nprmal execition only during development.
+  //   // Make sure the variable has a non-null value.
+  //   assert(text != null);
+
+  // // Make sure the value is less than 100.
+  //   assert(number < 100);
+
+  // // Make sure this is an https URL.
+  //   assert(urlString.startsWith('https'));
+
+  // * Assert with message, optional triling comma
+  // assert(urlString.startsWith('https'),
+  //     'URL ($urlString) should start with "https".');
+  var test = 1;
+  assert(test == 1);
+}
