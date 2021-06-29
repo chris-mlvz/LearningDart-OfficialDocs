7ca3dc5a255e29ecdbeef65bb596ee4289cbdd9d
chris_mlvz <chris.mlvz@gmail.com>
2021-06-29 15:11:39 -0500

    conditional expressions

---
+++ language_tour/operators/conditional_expessions.dart
@@ -0,0 +1,19 @@
+void main(List<String> args) {
+  // * condition ? expr1 : expr2 (ternary operator)
+  // var visibility = isPublic ? 'public' : 'private';
+
+  // * expr1 ?? expr2 (if non-null)
+  // var visibility = isPublic ? 'public' : 'private';
+
+  // * Slightly longer version uses ?: operator.
+  // String playerName(String? name) => name != null ? name : 'Guest';
+
+  // * Very long version uses if-else statement.
+  // String playerName(String? name) {
+  //   if (name != null) {
+  //     return name;
+  //   } else {
+  //     return 'Guest';
+  //   }
+  // }
+}
