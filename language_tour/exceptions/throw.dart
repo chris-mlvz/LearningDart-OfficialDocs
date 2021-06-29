20f9bd8f906bbba595ba02e1b1251fce13aeb380
chris_mlvz <chris.mlvz@gmail.com>
2021-06-29 18:50:05 -0500

    throw.dart

---
+++ language_tour/exceptions/throw.dart
@@ -0,0 +1,12 @@
+void main(List<String> args) {
+  // * Throwing or raising, an exeption.
+  // throw FormatException('Expected at least 1 section');
+
+  // * throw aritary objects
+  // throw 'Out of llamas!';
+
+  // * Productionquality code usually implement [Error] or [Exception].
+
+  // * Throw is an expresion, you can throw exeptions in => statements
+  // void distanceTo(Point other) => throw UnimplementedError();
+}
