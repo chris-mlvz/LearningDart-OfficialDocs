6cb6746bfb85dff08692c2776d048b93dd05d8ea
chris_mlvz <chris.mlvz@gmail.com>
2021-06-29 15:01:11 -0500

    logical operators

---
+++ language_tour/operators/logical_operators.dart
@@ -0,0 +1,12 @@
+void main(List<String> args) {
+  // ! Not.
+  // || OR.
+  // && AND.
+
+  // Example of using logical operators.
+  var col = 0;
+  var done = false;
+  if (!done && (col == 0 || col == 3)) {
+    // ...Do something...
+  }
+}
