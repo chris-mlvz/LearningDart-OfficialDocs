c9f451f3e2b70613790ef5a73b4dd9220accbf72
chris_mlvz <chris.mlvz@gmail.com>
2021-06-29 18:23:28 -0500

    for loops

---
+++ language_tour/control_flow_statements/for_loops.dart
@@ -0,0 +1,24 @@
+void main(List<String> args) {
+  // * Standard for loop
+  // var message = StringBuffer('Dart is fun');
+  // for (var i = 0; i < 5; i++) {
+  //   message.write('!');
+  // }
+  // print(message);
+
+  // * Closures inside of Dart’s for loops capture the value of the index
+  // var callbacks = [];
+  // for (var i = 0; i < 2; i++) {
+  //   callbacks.add(() => print(i));
+  // }
+  // callbacks.forEach((c) => c()); // 0 , 1
+
+  // * for-in
+  // for (var candidate in candidates) {
+  //   candidate.interview();
+  // }
+
+  // * forEach()
+  // var collection = [1, 2, 3];
+  // collection.forEach(print); // 1 2 3
+}
