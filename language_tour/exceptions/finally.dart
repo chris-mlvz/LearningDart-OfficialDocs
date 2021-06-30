b9aed85f41c6754ca0650dc50bd48647c3320353
chris_mlvz <chris.mlvz@gmail.com>
2021-06-29 19:04:35 -0500

    finally

---
+++ language_tour/exceptions/finally.dart
@@ -0,0 +1,18 @@
+void main(List<String> args) {
+  // * runs whetever or not an expetion is thrown. [finally] cluse.
+  // try {
+  //   breedMoreLlamas();
+  // } finally {
+  //   // Always clean up, even if an exception is thrown.
+  //   cleanLlamaStalls();
+  // }
+
+  // * runs after any matching [catch] clauses.
+  // try {
+  //   breedMoreLlamas();
+  // } catch (e) {
+  //   print('Error: $e'); // Handle the exception first.
+  // } finally {
+  //   cleanLlamaStalls(); // Then clean up.
+  // }
+}
