21f6aebe1cc1fded6e62b29f478799c2b2287262
chris_mlvz <chris.mlvz@gmail.com>
2021-07-01 14:20:56 -0500

    Specifying a library prefix.

---
+++ language_tour/libraries_and_visibility/specifying_a_library_prefix.dart
@@ -0,0 +1,10 @@
+/// If you import two libraries that have conflicting identifiers.
+/// You can specify a prefix for one or both libraries.
+// import 'package:lib1/lib1.dart';
+// import 'package:lib2/lib2.dart' as lib2;
+
+// // Uses Element from lib1.
+// Element element1 = Element();
+
+// // Uses Element from lib2.
+// lib2.Element element2 = lib2.Element();
