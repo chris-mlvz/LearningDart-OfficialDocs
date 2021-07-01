fa8f8f2e4396af8de441e8e70613595165afd280
chris_mlvz <chris.mlvz@gmail.com>
2021-07-01 14:16:52 -0500

    Using libraries.

---
+++ language_tour/libraries_and_visibility.dart/using_libraries.dart
@@ -0,0 +1,8 @@
+/// Using libraries
+///
+/// Use import to specify how a namespace from one library is used in the scope
+/// of another library.
+// import 'dart:html';
+
+/// The only required argument to import is a URI specifying the library.
+// import 'package:test/test.dart';
