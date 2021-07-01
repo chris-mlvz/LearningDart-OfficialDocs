692f22228693ad1daa9ce8e41c90f6c1858193ea
chris_mlvz <chris.mlvz@gmail.com>
2021-07-01 14:24:56 -0500

    Lazily loading a library.

---
+++ language_tour/libraries_and_visibility/lazilty_loading_a_library.dart
@@ -0,0 +1,11 @@
+/// Deferred loading (also called lazy loading) allows a web app to load a
+/// library on demand, if and when the library is needed.
+
+// ! Only dart2js supports deferred loading
+
+// import 'package:greetings/hello.dart' deferred as hello;
+
+// Future<void> greet() async {
+//   await hello.loadLibrary();
+//   hello.printGreeting();
+// }
