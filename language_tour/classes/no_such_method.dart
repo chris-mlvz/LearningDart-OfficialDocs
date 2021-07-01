5184375aedddfde48b636c7c950c4cd9e5910a76
chris_mlvz <chris.mlvz@gmail.com>
2021-07-01 12:12:21 -0500

    noSuchMethod()

---
+++ language_tour/classes/no_such_method.dart
@@ -0,0 +1,10 @@
+// * [noSuchMehod()] to detect attempts to use a non-existent method
+class A {
+  // Unless you override noSuchMethod, using a
+  // non-existent member results in a NoSuchMethodError.
+  @override
+  void noSuchMethod(Invocation invocation) {
+    print('You tried to use a non-existent member: '
+        '${invocation.memberName}');
+  }
+}
