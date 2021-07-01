14548a43bd6d47659b8aab5cd96a6695a32fe813
chris_mlvz <chris.mlvz@gmail.com>
2021-06-30 19:25:09 -0500

    Example: operators like instance methods.

---
+++ language_tour/classes/operators_instance_methods.dart
@@ -0,0 +1,23 @@
+// Example of usign an operator method.
+class Vector {
+  final int x, y;
+
+  Vector(this.x, this.y);
+
+  Vector operator +(Vector v) => Vector(x + v.x, y + v.y);
+  Vector operator -(Vector v) => Vector(x - v.x, y - v.y);
+
+  // Operator == and hashCode not shown.
+  // ···
+}
+
+void main() {
+  final v = Vector(2, 3);
+  final w = Vector(2, 2);
+  print((v + w).x);
+  print((v + w).y);
+  print((v - w).x);
+  print((v - w).y);
+  // assert(v + w == Vector(4, 5));
+  // assert(v - w == Vector(0, 1));
+}
