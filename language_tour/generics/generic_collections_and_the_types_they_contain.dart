26ca956c2fb519972ed39767faea2446e0d9c7bf
chris_mlvz <chris.mlvz@gmail.com>
2021-07-01 13:44:47 -0500

    Generic collections and the types they contain.

---
+++ language_tour/generics/generic_collections_and_the_types_they_contain.dart
@@ -0,0 +1,5 @@
+/// Dart generic types are reified.
+/// They carry their type information around at runtime.
+// var names = <String>[];
+// names.addAll(['Seth', 'Kathy', 'Lars']);
+// print(names is List<String>); // true
