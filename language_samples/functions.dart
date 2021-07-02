c38de1171a16b032299753a598b52e20d1b2e0bd
chris_mlvz <chris.mlvz@gmail.com>
2021-07-02 12:58:32 -0500

    Functions

---
+++ language_samples/functions.dart
@@ -0,0 +1,13 @@
+int fibonacci(int n) {
+  if (n == 0 || n == 1) return n;
+  return fibonacci(n - 1) + fibonacci(n - 2);
+}
+
+void main(List<String> args) {
+  var result = fibonacci(20);
+  print(result);
+
+  var flybyObjects = ['Jupiter', 'Saturn', 'Uranus', 'Neptune'];
+  flybyObjects.where((name) => name.contains('turn')).forEach(print);
+}
+
