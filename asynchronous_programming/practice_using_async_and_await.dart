0fb67275e85ac0a1735bbf4bfb6906002c56cae3
chris_mlvz <chris.mlvz@gmail.com>
2021-07-02 21:01:57 -0500

    Exercise: Practice using async and await.

---
+++ asynchronous_programming/practice_using_async_and_await.dart
@@ -0,0 +1,16 @@
+// // Part 1
+// // You can call the provided async function fetchRole()
+// // to return the user role.
+// Future<String> reportUserRole() async {
+//   var userRole = await fetchRole();
+//   return "User role: $userRole";
+// }
+
+// // Part 2
+// // Implement reportLogins here
+// // You can call the provided async function fetchLoginAmount()
+// // to return the number of times that the user has logged in.
+// Future reportLogins() async {
+//   var numberOfLogins = await fetchLoginAmount();
+//   return "Total number of logins: $numberOfLogins";
+// }
